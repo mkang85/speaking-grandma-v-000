f5cec1e22f8df1cf21ca8e6d93ec5063edfea755
Michael Kang <michaelkang85@gmail.com>
2018-02-04 01:42:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-
+ if string == 'I LOVE YOU GRANDMA!'
+   puts "I LOVE YOU TOO PUMPKIN!"
+ elsif string !== string.upcase
+   puts "HUH?! SPEAK UP, SONNY!"
+ else
+   puts "NO, NOT SINCE 1938!"
+ end
 end
